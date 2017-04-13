a06e081f88821839f071e054a972156a50e373ec
Lungu Andrei <h3nnry84@gmail.com>
2017-04-14 00:32:10 +0300

    Add TodoList

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Improve react'
+                },
+                {
+                    id: 2,
+                    text: 'Make React apps with laravel api'
+                },
+                {
+                    id: 3,
+                    text: 'Compare React with Angular'
+                },
+                {
+                    id: 4,
+                    text: 'Compare React with Vue.js'
+                }
+            ]
+        };
+    },
     render: function () {
+        var {todos} = this.state;
+
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         );
     }
