3d8b0a97eebdf7afb8e8a0d7e17df79af600668a
Lungu Andrei <h3nnry84@gmail.com>
2017-04-20 00:20:24 +0300

    Added filter functionality

--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -50,4 +50,49 @@ describe('TodoApi', () => {
             expect(TodoApi.getTodos()).toEqual(todos);
         });
     });
+
+    describe('filterTodos', () => {
+        var todos = [{
+            id:55,
+            text: 'test set',
+            completed: true
+        }, {
+            id:56,
+            text: 'other text',
+            completed: false
+        }, {
+            id:57,
+            text: 'test set',
+            completed: true
+        }];
+
+        it('should return all items if showCompled checked', () => {
+            var filteredTodos = TodoApi.filterTodos(todos, true, '');
+
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return only not completed  items if showCompled not checked', () => {
+            var filteredTodos = TodoApi.filterTodos(todos, false, '');
+
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        if('should sort completed status', () => {
+            var filteredTodos = TodoApi.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should sort todos by searchText', () => {
+            var filteredTodos = TodoApi.filterTodos(todos, true, 'test');
+
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoApi.filterTodos(todos, true, '');
+
+            expect(filteredTodos.length).toBe(3);
+        });
+    });
 });
