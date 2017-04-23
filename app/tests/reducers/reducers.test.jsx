cbb8643ed06a42dde7f9d63fb471a01267d2a6ac
Lungu Andrei <h3nnry84@gmail.com>
2017-04-24 01:44:47 +0300

    Add todo reducers and text

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -14,13 +14,46 @@ describe('Reducers', () => {
             expect(res).toEqual(action.searchText);
         });
 
-        if('should showCompleted status ge flipped', () => {
-            var action = {
-                type: 'TOGGLE_SHOW_COMLETED'
-            };
-            var res = reducers.showCompletedReducer(df(false), df(action));
+        describe('showCompletedReducer', () => {
+            if('should showCompleted status ge flipped', () => {
+                    var action = {
+                        type: 'TOGGLE_SHOW_COMLETED'
+                    };
+                    var res = reducers.showCompletedReducer(df(false), df(action));
 
-            expect(res).toEqual(true);
+                    expect(res).toEqual(true);
+                });
+        });
+
+        describe('todosReducer', () => {
+            it('should add new todo', () => {
+                var action = {
+                    type: 'ADD_TODO',
+                    text: 'Create new react app'
+                };
+                var res = reducers.todosReducer(df([]), df(action));
+
+                expect(res.length).toEqual(1);
+                expect(res[0].text).toEqual(action.text);
+            });
+
+            it('should toggle todo', () => {
+                var todos = [{
+                    id: '111',
+                    text: 'some text',
+                    completed: true,
+                    createdAt: '111',
+                    completedAt: '222'
+                }];
+                var action = {
+                    type: 'TOGGLE_TODO',
+                    id: '111'
+                };
+                var res = reducers.todosReducer(df(todos), df(action));
+
+                expect(res[0].completed).toEqual(false);
+                expect(res[0].completedAt).toEqual(undefined);
+            });
         });
     });
 });
