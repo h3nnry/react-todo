483c6646aa033fd4f4ade4b524d7e58c65564e91
Lungu Andrei <h3nnry84@gmail.com>
2017-05-04 02:24:17 +0300

    Move firebase credentials to env

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyCaWAcG5lpLqPXz5xwTiKMxDk64vlwhqSk",
-    authDomain: "h3nry-todo-app.firebaseapp.com",
-    databaseURL: "https://h3nry-todo-app.firebaseio.com",
-    projectId: "h3nry-todo-app",
-    storageBucket: "h3nry-todo-app.appspot.com",
-    messagingSenderId: "709804140302"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
