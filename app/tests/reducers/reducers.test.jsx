fd5752299542a43d7e8fdfba1862f6c8ffa10a39
Lungu Andrei <h3nnry84@gmail.com>
2017-05-06 16:41:33 +0300

    wipe todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -66,6 +66,7 @@ describe('Reducers', () => {
                 expect(res[0].completedAt).toEqual(updates.completedAt);
                 expect(res[0].text).toEqual(todos[0].text)
             });
+
             it('should add existing todos', () => {
                 var todos = [{
                     id: '123',
@@ -82,6 +83,22 @@ describe('Reducers', () => {
 
                 expect(res.length).toEqual(1);
                 expect(res[0]).toEqual(todos[0]);
+            });
+
+            it('should wipe todos on logout', () => {
+                var todos = [{
+                    id: '123',
+                    text: 'anyone',
+                    completed: false,
+                    createdAt: undefined,
+                    completedAt: 32000
+                }];
+                var action = {
+                    type: 'LOGOUT'
+                };
+                var res = reducers.todosReducer(df(todos), df(action));
+
+                expect(res.length).toEqual(0);
             });
         });
     });
